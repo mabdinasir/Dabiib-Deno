0fe5d755d507607450a58388b99d9073272aea1c
Mohamed Abdinasir <abdelnacerr@gmail.com>
2023-10-01 22:26:59 +1100

    remove cors temporarily

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,10 @@
 import "https://deno.land/std@0.173.0/dotenv/load.ts";
-import { Application, oakCors } from "./deps.ts";
+import { Application } from "./deps.ts";
 import router from "./routes/index.ts";
 
 const PORT = Deno.env.get("PORT") || 8082;
 
 const app = new Application();
-app.use(
-	oakCors({
-		// credentials: true,
-		origin: "*",
-		optionsSuccessStatus: 200,
-	}),
-);
 
 app.use(router.routes());
 
