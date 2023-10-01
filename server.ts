82e5701f724af10aaf9ade8eab2d1a905abdf014
Mohamed Abdinasir <abdelnacerr@gmail.com>
2023-10-01 16:08:04 +1100

    added a simple server and enabled Deno

---
+++ server.ts
@@ -0,0 +1,9 @@
+import { Application } from "https://deno.land/12.6.1/oak/mod.ts";
+
+const app = new Application();
+
+app.use((ctx) => {
+  ctx.response.body = "Hello World!";
+});
+
+await app.listen({ port: 8000 });
