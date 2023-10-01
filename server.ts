588e837e6742ea0f70ebfdfdf50c76a6c7d3be16
Mohamed Abdinasir <abdelnacerr@gmail.com>
2023-10-01 16:16:25 +1100

    simple hello world server

--- server.ts
+++ server.ts
@@ -1,9 +1,14 @@
-import { Application } from "https://deno.land/12.6.1/oak/mod.ts";
+import 'https://deno.land/std@0.173.0/dotenv/load.ts';
+import { Application } from "https://deno.land/x/oak/mod.ts";
 
-const app = new Application();
+const PORT = Deno.env.get('PORT') || 8082
+const app = new Application()
 
 app.use((ctx) => {
   ctx.response.body = "Hello World!";
 });
 
-await app.listen({ port: 8000 });
+
+console.log(`Server running on port ${PORT} `)
+
+await app.listen({ port: +PORT })
